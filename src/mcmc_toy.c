8e87a44e134b3aaa48c9c74aa5c1d3c5d6497121
Thomas Fisher-York <tly2@cornell.edu>
2015-03-19 15:55:30 -0400

    multidimensional (>2) tvd ...

--- src/mcmc_toy.c
+++ src/mcmc_toy.c
@@ -4,53 +4,46 @@
 #include <assert.h>
 #include "mcmc_toy.h"
 
-#define REFLECT 0
-
 int n_accept = 0;
 int n_reject = 0;
 
 // main:
 int main(){
-  int n_dimensions = 5; 
+  int n_dimensions = 3; 
   int Ngrid_max = 15;
   if(Ngrid_max % 2 == 0){ Ngrid_max++; } // if even add one, to make it be odd, in case it is not already
-  //  int gridsize = Ngrid_max + 1; // gridsize points, [0,Ngrid_max], gridsize will be even
-  //  int K = 23; // max number of gridpoints to move in 1 step
   int mcmc_steps  = 100000;
   int burn_in_steps = 20000;
-  //  Target_distribution* targp = initialize_pi(n_dimensions, Ngrid_max);
-  // printf("after initialize_pi\n");
-  // int k = 25;
-  for(int k=1; k<=2*(Ngrid_max+1); k = (int)(1.1*(k+1)) )
-    {
-      //     Histogram_2d* hist2d = initialize_histogram_2d(Ngrid_max);
-      //    printf("before initialize_histogram_ndim\n");
-      Histogram_ndim* hist_ndim = initialize_histogram_ndim(n_dimensions, Ngrid_max);
-      //        printf("after initialize_histogram_ndim\n");
-      Proposal proposal = {k, 3, 1.7};
+
+  int k = 1;
+    for(int k=1; k<=2*(Ngrid_max+1); k = (int)(1.1*(k+1)) ) // loop over different proposals
+    {    
+      Ndim_array_of_double* targp = initialize_ndim_array_of_double(n_dimensions, Ngrid_max+1, 0.0); // construct and init to zero
+            double sum_targp = set_ndim_array_of_double_with_function(targp, 1.0, f_1dim);
+
+            Histogram_ndim* hist_ndim = initialize_histogram_ndim(n_dimensions, Ngrid_max);
+      Proposal proposal = {k, 3, 1.5};
     
+      // ********** do burn-in ***********
       n_accept = 0;
       n_reject = 0;
       State* the_state = initialize_state(n_dimensions, Ngrid_max);
       for(int n=0; n<=burn_in_steps; n++){
-	//		printf("before and ... \n");
 	int x = mcmc_step_ndim(the_state, &proposal, Ngrid_max, hist_ndim);
-	//	printf("after mcmc_step_ndim n= %i\n", n);
       } 
+
+      // ********** do post-burn-in *********
       n_accept = 0;
       n_reject = 0;
-      //  hist2d = initialize_histogram_2d(Ngrid_max);
-      //    printf("before init hist ndim \n");
       hist_ndim = initialize_histogram_ndim(n_dimensions, Ngrid_max);
-      //     printf("after init hist ndim \n");
-      int xy_jumps = 0;
+      int n_jumps = 0;
       for(int n=1; n<=mcmc_steps; n++){   
 	// take a mcmc step
-	xy_jumps += mcmc_step_ndim(the_state, &proposal, Ngrid_max, hist_ndim);
+	n_jumps += mcmc_step_ndim(the_state, &proposal, Ngrid_max, hist_ndim);
 	//  printf("%8i %8i %8i %10.5g \n", n, the_state->point[0], the_state->point[1], the_state->prob);
       }
       double tvd = -1; //total_variation_distance(targp, hist_ndim);
-      printf("%8i  %8i  %8i  %10.6f   %8i  %8i  %8i \n", k, xy_jumps, mcmc_steps, tvd, n_accept, n_reject, n_accept+n_reject);
+      printf("%8i  %8i  %10.6f   %8i  %8i  %8i \n", k, n_jumps, tvd, n_accept, n_reject, n_accept+n_reject);
     }
 }
 // end of main
@@ -60,41 +53,30 @@ int main(){
 // initialize a state to a point chosen u.a.r. among the (Ngrid_max+1)^n_dimensionsoints
 State* initialize_state(int n_dimensions, int Ngrid_max){ // , Target_distribution* targp){
   State* the_state = (State*)malloc(1*sizeof(State));
-  //assert(n_dimensions == 2); // other dimensions not implemented at present.
   the_state->n_dimensions = n_dimensions;
   the_state->point = (int*)malloc(n_dimensions*sizeof(int));
   for(int i=0; i<n_dimensions; i++){
     the_state->point[i] = (int)( drand() * (Ngrid_max+1) );
   }
   the_state->prob = F(n_dimensions, the_state->point, Ngrid_max); // targp->pi[the_state->point[0]][the_state->point[1]];
-  //  the_state->xhalf = (the_state->point[0] <= Ngrid_max/2)? 0 : 1;
-  //  the_state->yhalf = (the_state->point[1] <= Ngrid_max/2)? 0 : 1;
   return the_state;
 }
 
 
 int mcmc_step_ndim(State* the_state, Proposal* prop, int Ngrid_max, Histogram_ndim* hist_ndim){
   int Ndim = hist_ndim->Ndim;
-  //  int prop_width = (prop->p1 >= 1 || drand() < prop->p1)? prop->W1 : prop->W2; 
-  //  int Ngrid_max = targp->Ngrid_max;
   int* index_array = the_state->point;
-  // printf("point: ");
   int* prop_index_array = propose(Ndim, index_array, prop);
-  //  printf("after propose \n");
-  // print_int_array(Ndim, index_array);
-  // print_int_array(Ndim, prop_index_array);
-  int n_jumps = 0; // 0 or 1
   double p_of_proposed_state = F(Ndim, prop_index_array, Ngrid_max);
-  //  printf("prob of proposed state: %12.6g \n", p_of_proposed_state);
-  // f( (double)iprop/(double)Ngrid_max ) * f( (double)jprop/(double)Ngrid_max );
   double p_of_current_state = the_state->prob;
+  int n_jumps = 0;
   // prop ratio is 1 for symmetric proposal
   if((p_of_proposed_state > p_of_current_state) || (p_of_proposed_state > drand() * p_of_current_state)){ // accept
-    // count if jumped from < 0.5 to > 0.5 in x and y
-    int h0max = Ngrid_max/2;
+  
+    int h0max = Ngrid_max/2;   // count if jumped from < 0.5 to > 0.5 in x and y
     for(int i=0; i<Ndim; i++){
-      //    printf("i: %i \n", i);
-      if( ( the_state->point[i] <= h0max  && prop_index_array[i] > h0max ) || ( the_state->point[i] > h0max  &&  prop_index_array[i] <= h0max ) ){ 
+      if( ( the_state->point[i] <= h0max  && prop_index_array[i] > h0max ) 
+	  || ( the_state->point[i] > h0max  &&  prop_index_array[i] <= h0max ) ){ 
 	n_jumps++;
       }
     }
@@ -102,22 +84,14 @@ int mcmc_step_ndim(State* the_state, Proposal* prop, int Ngrid_max, Histogram_nd
     the_state->point = prop_index_array;
     the_state->prob = p_of_proposed_state;
     n_accept++;
-    // printf("%i %i   %i %i   %i %i \n", the_state->point[0], the_state->point[1], iprop, jprop, x_jumps, y_jumps);
   }else{ 
     // reject proposed move.
     n_reject++;
     free(prop_index_array);
   }
-  // ((int*)hist_ndim->bin_count[the_state->point[0]].array)[the_state->point[1]]++;
-  //  printf("after step, before get_bin_pointer.\n");
-  // print_int_array(Ndim, index_array);
-  //  print_int_array(Ndim, prop_index_array);
-  //printf("before get_bin_pointer \n");
   int* bp = get_bin_pointer(hist_ndim->bin_count, the_state->point); 
-  //  printf("after get_bin_pointer \n");
   (*bp)++;
   hist_ndim->total_count++;
-  //    printf("%8i %8i %10.5g \n", the_state->point[0], the_state->point[1], the_state->prob);
   return n_jumps;
 }
 
@@ -138,12 +112,12 @@ double F(int Ndim, int* index_array, int Ngrid_max){
     //  printf("i, index, Ngrid_max: %i %i %i \n", i, index, Ngrid_max);
     if(index < 0  || index > Ngrid_max){ return 0.0; }
     double x = (double)index/(double)Ngrid_max;
-    result *= f(x);
+    result *= f_1dim(x);
   }
   return result;
 }    
 
-double f(double x){
+double f_1dim(double x){
   if(x<0.0 || x > 1.0){ // out of bounds, return 0
     return 0.0;
   }
@@ -194,6 +168,113 @@ Ndim_array_of_double* initialize_ndim_array_of_double(int Ndim, int Nsize, doubl
   return array_struct;
 }
 
+double set_ndim_array_of_double_with_function(Ndim_array_of_double* array_struct,
+						     double outer_value, double_func_ptr function){
+  // for probs. of form Prod(i=1,ndim)(f(x_i)), give it a 1-dim function of x (in [0,1]) as the last argument
+  int Ndim = array_struct->Ndim;
+  int Nsize = array_struct->Nsize;
+  double sum = 0.0;
+  if(Ndim == 1){
+    for(int i=0; i<Nsize; i++){
+      double x = (double)i/(double)(Nsize-1);
+      //    printf("%8i %8.5g  %8.5g \n", i, outer_value, function(x));
+      double innermost_value =  outer_value*function(x);
+      ((double*)array_struct->array)[i] = innermost_value;
+      sum += innermost_value;
+      //   printf("%8.5f ", innermost_value);
+    }//printf("\n");
+  }else{    
+    for(int i=0; i<Nsize; i++){   
+      double x = (double)i/(double)(Nsize-1);
+      Ndim_array_of_double* inner_array_struct = ((Ndim_array_of_double**)array_struct->array)[i];
+      sum +=  set_ndim_array_of_double_with_function(inner_array_struct, outer_value*function(x), function);
+    }
+  }
+  return sum;
+}
+
+void normalize_pdf_ndim(Discrete_pdf_ndim* pdf){
+  // for probs. of form Prod(i=1,ndim)(f(x_i)), give it a 1-dim function of x (in [0,1]) as the last argument
+  /* int Ndim = pdf->Ndim; */
+  /* int Nsize = pdf->Nsize; */
+  /* int ndim_array = pdf->probs; */
+  multiply_ndim_array_of_double_by_scalar(pdf->probs, 1/(pdf->sum));
+  pdf->sum = 1.0; // normalized now, so sum is 1.0
+}
+
+void multiply_ndim_array_of_double_by_scalar(Ndim_array_of_double* array_struct, double multiplier){
+  int Ndim = array_struct->Ndim;
+  int Nsize = array_struct->Nsize;
+  if(Ndim == 1){
+    for(int i=0; i<Nsize; i++){
+      ((double*)array_struct->array)[i] *= multiplier;
+    }
+  }else{    
+    for(int i=0; i<Nsize; i++){   
+      Ndim_array_of_double* inner_array_struct = ((Ndim_array_of_double**)array_struct->array)[i];
+      multiply_ndim_array_of_double_by_scalar(inner_array_struct, multiplier);
+    }
+  }
+}
+
+
+double sum_ndim_array_of_double(Ndim_array_of_double* array_struct){
+  int Ndim = array_struct->Ndim;
+  int Nsize = array_struct->Nsize;
+  double sum = 0.0;
+  if(Ndim == 1){
+    //double sum = 0.0;
+    double* the_array = (double*)array_struct->array;
+    for(int i=0; i<Nsize; i++){
+      sum += the_array[i];
+    }
+    return sum; 
+  }else{
+    //double sum = 0.0;
+    Ndim_array_of_double* the_array = (Ndim_array_of_double*)array_struct->array;
+    for(int i=0; i<Nsize; i++){
+      sum += sum_ndim_array_of_double(the_array);
+    }
+  }
+  return sum;
+}
+
+double sum_abs_difference_ndim_arrays_of_double(Ndim_array_of_double* a1, Ndim_array_of_double* a2){
+  int Ndim = a1->Ndim;
+  int Nsize = a1->Nsize;
+  assert(a2->Ndim == Ndim   &&  a2->Nsize == Nsize);
+  double sum_abs_difference = 0.0;
+  if(Ndim == 1){
+    //double sum = 0.0;
+    double* A1 = (double*)a1->array;
+    double* A2 = (double*)a2->array;
+    for(int i=0; i<Nsize; i++){
+      sum_abs_difference += abs(A1[i] - A2[i]);
+    }
+    //   return sum_abs_difference; 
+  }else{
+    //double sum = 0.0;
+    Ndim_array_of_double* A1 = (Ndim_array_of_double*)a1->array;
+    Ndim_array_of_double* A2 = (Ndim_array_of_double*)a2->array;
+    for(int i=0; i<Nsize; i++){
+      sum_abs_difference += sum_abs_difference_ndim_arrays_of_double(A1, A2);
+    }
+  }
+  return sum_abs_difference;
+}
+
+Discrete_pdf_ndim* init_target_distribution(int Ndim, int Ngrid_max, int normalize){
+  Discrete_pdf_ndim* targ_pdf = (Discrete_pdf_ndim*)malloc(sizeof(Discrete_pdf_ndim));
+  targ_pdf->Ndim = Ndim;
+  targ_pdf->Ngrid_max = Ngrid_max;
+  targ_pdf->probs = initialize_ndim_array_of_double(Ndim, Ngrid_max+1, 0.0);
+  double targ_pdf_sum = set_ndim_array_of_double_with_function(targ_pdf->probs, 1.0, f_1dim);
+  if(normalize){
+    normalize_pdf_ndim(targ_pdf);
+  }
+}
+
+
 // ((int*)hist_ndim->bin_count[the_state->point[0]].array)[the_state->point[1]]++;
 int* get_bin_pointer(Ndim_array_of_int* A, int* index_array){
   int Ndim = A->Ndim;
@@ -234,28 +315,12 @@ void print_int_array(int Nsize, int* array){
   }printf("\n");
 }
 
-
-double total_variation_distance(Target_distribution* targp, Histogram_ndim* hist_ndim){
-  int Ngrid_max = targp->Ngrid_max;
-  double tvd = 0;
-  int point[2] = {0, 0};
-  for(int i=0; i<= Ngrid_max; i++){
-  point[0] = i;
-  for(int j=0; j<= Ngrid_max; j++){
-  point[1] = j;
-  int* bp = get_bin_pointer(hist_ndim->bin_count, point);
-  int bin_count = *bp;
-  double mcmc_prob = (double)bin_count/(double)hist_ndim->total_count;
-  //         printf("%8.4f %8.4f %8.4f \n", mcmc_prob, targp->pi[i][j], tvd);
-  tvd += fabs(mcmc_prob - targp->pi[i][j]);
-  }
-  }
-  tvd *= 0.5;
-  return tvd;
-  }
-
-
-
+double total_variation_distance(Discrete_pdf_ndim* targp, Discrete_pdf_ndim* mcmc_out){
+  // both distributions must be normalized ahead of time
+  Ndim_array_of_double* a1 = targp->probs;
+  Ndim_array_of_double* a2 = mcmc_out->probs;
+  double tvd = 0.5 * sum_abs_difference_ndim_arrays_of_double(targp->probs, mcmc_out->probs);
+}
 
 // *********** obsolete ? stuff ********************
 
@@ -264,64 +329,64 @@ int propose_1dim(int i, int Width, int Ngrid_max){ // 1 dimensional proposal
   delta_i = (int)((2*Width+1)*drand()) - Width;
   int iprop = i + delta_i; 
   //  printf("width, deltai, iprop: %8i %8i %8i \n", Width, delta_i, iprop);
-  if(REFLECT){
-    while(iprop < 0 || iprop > Ngrid_max){
-      if(iprop < 0){
-	iprop = -iprop;
-      }
-      if(iprop > Ngrid_max){
-	iprop = Ngrid_max - (iprop - Ngrid_max); // so, e.g. Ngrid_max+1 => Ngrid_max-1, etc.
-      }
-    }
-  }else{
-    // just go ahead and propose something out of bounds - it will have pi = 0, so won't go there.
-  }
+  /* if(REFLECT){ */
+  /*   while(iprop < 0 || iprop > Ngrid_max){ */
+  /*     if(iprop < 0){ */
+  /* 	iprop = -iprop; */
+  /*     } */
+  /*     if(iprop > Ngrid_max){ */
+  /* 	iprop = Ngrid_max - (iprop - Ngrid_max); // so, e.g. Ngrid_max+1 => Ngrid_max-1, etc. */
+  /*     } */
+  /*   } */
+  /* }else{ */
+  /*   // just go ahead and propose something out of bounds - it will have pi = 0, so won't go there. */
+  /* } */
   return iprop;
 }
 
 // int mcmc_step(State* the_state, Proposal* prop, Target_distribution* targp, Histogram_2d* hist2d){
-int mcmc_step_2d(State* the_state, Proposal* prop, int Ngrid_max, Histogram_ndim* hist_ndim){
-  int prop_width = (prop->p1 >= 1 || drand() < prop->p1)? prop->W1 : prop->W2; 
-  //  int Ngrid_max = targp->Ngrid_max;
-  int i = the_state->point[0];
-  int j = the_state->point[1];
-  int iprop, jprop;
-  while(1){ // don't propose same point
-    iprop = propose_1dim(i, prop_width, Ngrid_max);
-    jprop = propose_1dim(j, prop_width, Ngrid_max);
-    if(iprop != i || jprop != j) break;
-  }
-  int x_jumps = 0; // 0 or 1
-  int y_jumps = 0; // 0 or 1
-  double p_of_proposed_state = f( (double)iprop/(double)Ngrid_max ) * f( (double)jprop/(double)Ngrid_max );
-  double p_of_current_state = the_state->prob;
-  // prop ratio is 1 for symmetric proposal
-  if((p_of_proposed_state > p_of_current_state) || (p_of_proposed_state > drand() * p_of_current_state)){ // accept
-    // count if jumped from < 0.5 to > 0.5 in x and y
-    int h0max = Ngrid_max/2;
-    if( ( the_state->point[0] <= h0max  && iprop > h0max ) || ( the_state->point[0] > h0max  &&  iprop <= h0max ) ){ 
-      x_jumps++;
-    }
-    if( ( the_state->point[1] <= h0max  && jprop > h0max ) || ( the_state->point[1] > h0max  &&  jprop <= h0max ) ){ 
-      y_jumps++;
-    }
-    // set state to the proposed state:
-    the_state->point[0] = iprop; 
-    the_state->point[1] = jprop;
-    the_state->prob = p_of_proposed_state;
-    n_accept++;
-    // printf("%i %i   %i %i   %i %i \n", the_state->point[0], the_state->point[1], iprop, jprop, x_jumps, y_jumps);
-  }else{ 
-    // reject proposed move; do nothing
-    n_reject++;
-  }
-  // ((int*)hist_ndim->bin_count[the_state->point[0]].array)[the_state->point[1]]++;
-  int* bp = get_bin_pointer(hist_ndim->bin_count, the_state->point); 
-  (*bp)++;
-  hist_ndim->total_count++;
-  //    printf("%8i %8i %10.5g \n", the_state->point[0], the_state->point[1], the_state->prob);
-  return x_jumps + y_jumps;
-}
+/* int mcmc_step_2d(State* the_state, Proposal* prop, int Ngrid_max, Histogram_ndim* hist_ndim){ */
+/*   int prop_width = (prop->p1 >= 1 || drand() < prop->p1)? prop->W1 : prop->W2;  */
+/*   //  int Ngrid_max = targp->Ngrid_max; */
+/*   int i = the_state->point[0]; */
+/*   int j = the_state->point[1]; */
+/*   int iprop, jprop; */
+/*   while(1){ // don't propose same point */
+/*     iprop = propose_1dim(i, prop_width, Ngrid_max); */
+/*     jprop = propose_1dim(j, prop_width, Ngrid_max); */
+/*     if(iprop != i || jprop != j) break; */
+/*   } */
+/*   int x_jumps = 0; // 0 or 1 */
+/*   int y_jumps = 0; // 0 or 1 */
+/*   double p_of_proposed_state = f_1dim( (double)iprop/(double)Ngrid_max ) * f_1dim( (double)jprop/(double)Ngrid_max ); */
+/*   double p_of_current_state = the_state->prob; */
+/*   // prop ratio is 1 for symmetric proposal */
+/*   if((p_of_proposed_state > p_of_current_state) || (p_of_proposed_state > drand() * p_of_current_state)){ // accept */
+/*     // count if jumped from < 0.5 to > 0.5 in x and y */
+/*     int h0max = Ngrid_max/2; */
+/*     if( ( the_state->point[0] <= h0max  && iprop > h0max ) || ( the_state->point[0] > h0max  &&  iprop <= h0max ) ){  */
+/*       x_jumps++; */
+/*     } */
+/*     if( ( the_state->point[1] <= h0max  && jprop > h0max ) || ( the_state->point[1] > h0max  &&  jprop <= h0max ) ){  */
+/*       y_jumps++; */
+/*     } */
+/*     // set state to the proposed state: */
+/*     the_state->point[0] = iprop;  */
+/*     the_state->point[1] = jprop; */
+/*     the_state->prob = p_of_proposed_state; */
+/*     n_accept++; */
+/*     // printf("%i %i   %i %i   %i %i \n", the_state->point[0], the_state->point[1], iprop, jprop, x_jumps, y_jumps); */
+/*   }else{  */
+/*     // reject proposed move; do nothing */
+/*     n_reject++; */
+/*   } */
+/*   // ((int*)hist_ndim->bin_count[the_state->point[0]].array)[the_state->point[1]]++; */
+/*   int* bp = get_bin_pointer(hist_ndim->bin_count, the_state->point);  */
+/*   (*bp)++; */
+/*   hist_ndim->total_count++; */
+/*   //    printf("%8i %8i %10.5g \n", the_state->point[0], the_state->point[1], the_state->prob); */
+/*   return x_jumps + y_jumps; */
+/* } */
 
 
 /*
@@ -355,20 +420,20 @@ int mcmc_step_2d(State* the_state, Proposal* prop, int Ngrid_max, Histogram_ndim
 */
 
 
-Histogram_2d* initialize_histogram_2d(int Ngrid_max){
-  Histogram_2d* histogram = (Histogram_2d*)malloc(1*sizeof(Histogram_2d));
-  histogram->Ngrid_max = Ngrid_max;
-  histogram->total_count = 0;
-  int** bin_count = (int**)malloc( (Ngrid_max+1) * sizeof(double*) );
-  for(int i=0; i<=Ngrid_max; i++){
-    bin_count[i] = (int*)malloc( (Ngrid_max+1) * sizeof(double) );
-  }
+/* Histogram_2d* initialize_histogram_2d(int Ngrid_max){ */
+/*   Histogram_2d* histogram = (Histogram_2d*)malloc(1*sizeof(Histogram_2d)); */
+/*   histogram->Ngrid_max = Ngrid_max; */
+/*   histogram->total_count = 0; */
+/*   int** bin_count = (int**)malloc( (Ngrid_max+1) * sizeof(double*) ); */
+/*   for(int i=0; i<=Ngrid_max; i++){ */
+/*     bin_count[i] = (int*)malloc( (Ngrid_max+1) * sizeof(double) ); */
+/*   } */
 
-  for(int i=0; i<=Ngrid_max; i++){
-    for(int j=0; j<=Ngrid_max; j++){
-      bin_count[i][j] = 0;
-    }
-  }
-  histogram->bin_count = bin_count;
-  return histogram;
-}
+/*   for(int i=0; i<=Ngrid_max; i++){ */
+/*     for(int j=0; j<=Ngrid_max; j++){ */
+/*       bin_count[i][j] = 0; */
+/*     } */
+/*   } */
+/*   histogram->bin_count = bin_count; */
+/*   return histogram; */
+/* } */
--- src/mcmc_toy.h
+++ src/mcmc_toy.h
@@ -1,4 +1,7 @@
 // typedefs:
+typedef int (*int_func_ptr)(); // don't need argument lists here?
+typedef double (*double_func_ptr)();
+
 typedef struct
 {
   int n_dimensions;
@@ -49,20 +52,6 @@ typedef struct
   void* array; // an N dim array of N-1 dim arrays (or of double if N=1) 
 } Ndim_array_of_int;
 
-typedef struct
-{
-  double sum; 
-  int n_dimensions; // must be 2 for now.
-  int Ngrid_max; // grid [0,Ngrid_max] each dimension
-  Ndim_array_of_double* pi; // pi[i][j] is probability (unnormalized) of
-} Target_distribution;
-
-typedef struct
-{
-  int Ngrid_max; // grid [0,Ngrid_max] each dimension
-  int total_count;
-  int** bin_count; //
-} Histogram_2d;
 
 typedef struct
 {
@@ -77,23 +66,44 @@ typedef struct
   int Ndim; 
   int Ngrid_max; // grid [0,Ngrid_max] each dimension
   int sum;
-  Ndim_array_of_double* bin_count; //
+  Ndim_array_of_double* probs; //
 } Discrete_pdf_ndim;
 
 // function declarations:
 State* initialize_state(int n_dimensions, int Ngrid_max); //, Target_distribution* targp);
-int mcmc_step_2d(State* the_state, Proposal* prop, int Ngrid_max, Histogram_ndim* hist_ndim);
+//int mcmc_step_2d(State* the_state, Proposal* prop, int Ngrid_max, Histogram_ndim* hist_ndim);
 int mcmc_step_ndim(State* the_state, Proposal* prop, int Ngrid_max, Histogram_ndim* hist_ndim);
-double f(double x);
+double f_1dim(double x);
 double F(int Ndim, int* index_array, int Ngrid_max);
 int propose_1dim(int i, int Width, int Ngrid_max);
 int* propose(int Ndim, int* index_array, Proposal* prop);
 double drand(void);
 // Target_distribution* initialize_pi(int n_dimensions, int Ngrid_max);
-Histogram_2d* initialize_histogram_2d(int Ngrid_max);
+//Histogram_2d* initialize_histogram_2d(int Ngrid_max);
 Histogram_ndim* initialize_histogram_ndim(int Ndim, int Ngrid_max);
 // double total_variation_distance(Target_distribution* targp, Histogram_ndim* hist_ndim);
 Ndim_array_of_double* initialize_ndim_array_of_double(int Ndim, int Nsize, double init_value);
 Ndim_array_of_int* initialize_ndim_array_of_int(int Ndim, int Nsize, int init_value);
 int* get_bin_pointer(Ndim_array_of_int* A, int* index_array);
 void print_int_array(int Nsize, int* array);
+double sum_ndim_array_of_double(Ndim_array_of_double* array_struct);
+double set_ndim_array_of_double_with_function(Ndim_array_of_double* array_struct,
+						     double outer_value, double_func_ptr function);
+
+void multiply_ndim_array_of_double_by_scalar(Ndim_array_of_double* array_struct, double multiplier);
+void normalize_pdf_ndim(Discrete_pdf_ndim* pdf);
+double total_variation_distance(Discrete_pdf_ndim* targp, Discrete_pdf_ndim* mcmc_out);
+double sum_abs_difference_ndim_arrays_of_double(Ndim_array_of_double* a1, Ndim_array_of_double* a2);
+/* typedef struct */
+/* { */
+/*   double sum;  */
+/*   int n_dimensions; // must be 2 for now. */
+/*   int Ngrid_max; // grid [0,Ngrid_max] each dimension */
+/*   Ndim_array_of_double* pi; // pi[i][j] is probability (unnormalized) of */
+/* } Target_distribution; */
+/* typedef struct */
+/* { */
+/*   int Ngrid_max; // grid [0,Ngrid_max] each dimension */
+/*   int total_count; */
+/*   int** bin_count; // */
+/* } Histogram_2d; */
