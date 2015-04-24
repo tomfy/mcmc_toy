d0175a7247ea51cb25f2c76072b7244a3f3ea0a8
Thomas Fisher-York <tly2@cornell.edu>
2015-04-24 16:52:22 -0400

    MULTI as param means will loop over several values for that param.

--- src/run_mcmc_toy.pl
+++ src/run_mcmc_toy.pl
@@ -13,32 +13,19 @@ my $epsilon = 1e-6;
 # analysis parameters
 # n_bins
 
-
-# GetOptions(
-# 	   'n_dimensions=s'           => \$gg_filename, #
-# 	   'nj!'          => \$do_nj, # whether to do NJ tree construction for actual data
-# 	   #  exclamation point means can use  -nj  and  -nonj
-# 	   'ft!'      => \$do_ft, # whether to do FastTree for actual data
-# 	   'phyml!' => \$do_phyml, # whether to do Phyml tree construction for actual data
-# 	   'support!'          => \$support, # -nosupport to turn off outputting of local branch support numbers.
-# 	   'n_nj_bs=i' => \$n_nj_bs, # number of NJ bootstraps to do
-# 	   'n_ft_bs=i' => \$n_ft_bs, # number of FastTree bootstraps to do
-# 	   'n_phyml_bs=i' => \$n_phyml_bs, # number of Phyml bootstraps to do
-# 	   #    'ml_bs!' => \$ml_bs, # boolean to do ML bs or not (default: 0)
-# 	   'species_tree=s' => \$species_tree_newick_file, 
-
 my $n_generations = 100000;
 my $n_burn_in = 0;
-my $n_replicates = 1;		# > 1 not implemented 
+#my $n_replicates = 1; # > 1 not implemented 
 my $n_dimensions = 1;
 my $peaks_string = '-0.5, 0.1, 0.5; 0.5, 0.1, 0.5';
 my $n_peaks;
 my $temperatures_string = '1.0';
-my $proposals_string = 'gaussian, SIGMA1, 1.4, 1.0'; # if multiple proposals, separate with ; e.g. 'gaussian 0.07 1.5 0.9; gaussian ... '
-my %n_bins_hash = (1 => 4096, 2 => 64, 3 => 16, 4 => 8);
-my $n_bins = $n_bins_hash{$n_dimensions};
-my $n_bins_1d = 216;
+my $proposals_string = 'gaussian, 1.0, 1.4, 1.0'; # if multiple proposals, separate with ; e.g. 'gaussian 0.07 1.5 0.9; gaussian ... '
+#my %n_bins_hash = (1 => 4096, 2 => 64, 3 => 16, 4 => 8);
+my $n_bins = 4;			# $n_bins_hash{$n_dimensions};
+my $n_bins_1d = 360;
 my $chain_type = "mcmc";	# or "iid"
+my $n_reps = 8;
 GetOptions(
 	   'n_dimensions=i' => \$n_dimensions,
 	   'peaks=s' => \$peaks_string, # e.g. '-0.5,0.05,0.5;0.5,0.05,0.5'  -> 2 peaks
@@ -49,16 +36,28 @@ GetOptions(
 	   'n_generations=i' => \$n_generations,
 	   'n_burnin=i' => \$n_burn_in,
 	   'type=s' => \$chain_type,
+	   'n_reps=i' => \$n_reps,
 	  );
 
-
+my %ndim_sig_opt_deltafunction = (1 => 1.000, 2 => 0.8121, 3 => 0.7190, 4 => 0.6541, 5 => 0.5995,
+				  6 => 5454, 7 => 0.4811, 8 => 0.4075, 9 => 0.3597, 10 => 0.0.3303,
+				  11 => 0.3095, 12 => 0.2934, 13 => 0.2802, 14 => 0.2690, 15 => 0.2593,
+				  16 => 0.2507, 17 => 0.2429, 18 => 0.2360, 19 => 0.2296, 20 => 0.2237,
+				 );
 my @peak_strings = split(";", $peaks_string);
 $n_peaks = scalar @peak_strings;
 for (@peak_strings) {
   s/,/ /g;
 }
+my $target_peak_sigma = 0.1;
+if ($peak_strings[0] =~ /^\s*(\S+)\s+(\S+)/) {
+  $target_peak_sigma = $2;
+}
 # print "# proposals string: $proposals_string \n";
 
+my $prop_sig_1 = $ndim_sig_opt_deltafunction{$n_dimensions}; # 'opt' peak jumping sigma (peak spacing of 1)
+my $prop_sig_2 = 2.38*$target_peak_sigma/sqrt($n_dimensions); # 'opt' withing peak sigma
+my $prop_p1 = 0.9;
 $temperatures_string =~ s/[,;]/ /g;
 my @temperatures = split(" ", $temperatures_string);
 my $n_temperatures = scalar @temperatures;
@@ -72,14 +71,14 @@ while (scalar @proposal_strings < scalar @temperatures) {
 
 my $proto_arg_string = "$n_dimensions  $n_peaks  ";
 $proto_arg_string .= join("  ", @peak_strings) . "  ";
-$proto_arg_string .= "$n_burn_in NGENERATIONS $n_replicates ";
+$proto_arg_string .= " $n_generations  ";
 $proto_arg_string .= "$n_temperatures ";
 for my $i (0..$n_temperatures-1) {
   my $T = $temperatures[$i];
   $proto_arg_string .= "$T " . $proposal_strings[$i] . "  ";
 }
 $proto_arg_string .= "$n_bins  $n_bins_1d  ";
-#print "$n_generations ;;; $proto_arg_string \n"; exit;
+# print "$n_generations ;;; $proto_arg_string \n"; exit;
 # my $seed = srand();
 # $proto_arg_string .= "$seed ";
 
@@ -94,46 +93,48 @@ $proto_arg_string .= "$n_bins  $n_bins_1d  ";
 #my $tvd_limit = 0.05 ;
 my $dmu_sq_limit_f = 0.001;
 my $dmu_sq_limit;
-my $n_reps = 25;
+# my $n_reps = 8;
 srand();
 
 my $sig1 = 1.0;
 my $the_arg_string;
-while (1) {
-  $the_arg_string = $proto_arg_string;
-  $the_arg_string =~ s/NGENERATIONS/$n_generations/;
-  $the_arg_string =~ s/SIGMA1/$sig1/;
-  my $seed = int(rand(1000000));
-  my $mcmc_toy_out =  `~/mcmc_toy/src/mcmc_toy $the_arg_string $seed mcmc`;
-  #  print "$mcmc_toy_out \n";
-  my $last_line = `tail -1 tvd_vs_gen`;
-  #  exit;
-  my @cols = split(" ", $last_line);
-  my $ngens = shift @cols;
-  my $p_accept = shift @cols;
-  my $dmusq = shift @cols;
+# while (0) {
+#   $the_arg_string = $proto_arg_string;
+#   $the_arg_string =~ s/NGENERATIONS/$n_generations/;
+#   $the_arg_string =~ s/SIGMA1/$sig1/;
+#   $the_arg_string =~ s/SIGMA2/$prop_sig_2/;
+#   $the_arg_string =~ s/PROP_P1/$prop_p1/;
+#   my $seed = int(rand(1000000));
+#   my $mcmc_toy_out =  `~/mcmc_toy/src/mcmc_toy $the_arg_string $seed mcmc`;
+#   #  print "$mcmc_toy_out \n";
+#   my $last_line = `tail -1 tvd_vs_gen`;
+#   #  exit;
+#   my @cols = split(" ", $last_line);
+#   my $ngens = shift @cols;
+#   my $p_accept = shift @cols;
+#   my $dmusq = shift @cols;
 
-  my $run_params = `cat run_params`;
-  my @runparam_lines = split("\n", $run_params);
-  my ($T, $shape, $s1, $s2, $p1);
-  my ($mean, $variance);
+#   my $run_params = `cat run_params`;
+#   my @runparam_lines = split("\n", $run_params);
+#   my ($T, $shape, $s1, $s2, $p1);
+#   my ($mean, $variance);
  
-  for (@runparam_lines) {
-    if (/T, Proposal\S+:\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/) {
-      ($T, $shape, $s1, $s2, $p1) = ($1, $2, $3, $4, $5);
-    } elsif(/target\s+mean,\s*variance:\s+(\S+)\s+(\S+)/){
-      ($mean, $variance) = ($1, $2);
-    }
-  }
- $dmu_sq_limit = $dmu_sq_limit_f * $variance;
-# print "mean, variance: $mean $variance dmusq: $dmusq\n";
-#exit;
+#   for (@runparam_lines) {
+#     if (/T, Proposal\S+:\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/) {
+#       ($T, $shape, $s1, $s2, $p1) = ($1, $2, $3, $4, $5);
+#     } elsif (/target\s+mean,\s*variance:\s+(\S+)\s+(\S+)/) {
+#       ($mean, $variance) = ($1, $2);
+#     }
+#   }
+#   $dmu_sq_limit = $dmu_sq_limit_f * $variance;
+#   # print "mean, variance: $mean $variance dmusq: $dmusq\n";
+#   #exit;
 
-  # my @mcmc_1d_tvds = @tvds[3 .. 3+$n_dimensions-1];
-  # my $avg_1d_tvd = mean(@mcmc_1d_tvds);
-  last if($dmusq < $dmu_sq_limit);
-  $n_generations = int( 1.2*$n_generations*($dmusq/$dmu_sq_limit) );
-}
+#   # my @mcmc_1d_tvds = @tvds[3 .. 3+$n_dimensions-1];
+#   # my $avg_1d_tvd = mean(@mcmc_1d_tvds);
+#   last if($dmusq < $dmu_sq_limit);
+#   $n_generations = int( 1.2*$n_generations*($dmusq/$dmu_sq_limit) );
+# }
 #exit;
 my @sig_1s = (0.14, 0.20, 0.3, 0.45, 0.6, 0.8, 1.0, 1.2, 1.4, 1.7, 2.0, 4.0, 7.0);
 # @sig_1s = (0.2, 0.45, 0.7, 1.0, 1.4);
@@ -142,132 +143,120 @@ my @sig_1s = (0.14, 0.20, 0.3, 0.45, 0.6, 0.8, 1.0, 1.2, 1.4, 1.7, 2.0, 4.0, 7.0
 @sig_1s = (0.15, 0.25, 0.5, 0.8, 1.0, 1.25, 1.6, 2.3, 3.8, 7.0);
 @sig_1s = (0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4);
 @sig_1s = (0.55, 0.7, 0.9, 1.1, 1.3, 1.6, 2.0, 5.0);
-@sig_1s = (3.0, 4.0, 7.0);
+@sig_1s = (0.4, 1.0, 3.0);
 
-@sig_1s = (0.128, 0.164, 0.2, 0.256, 0.328, 0.4, 0.512, 0.676, 0.8, 1.0, 1.28, 1.64, 2.0, 3.28, 4.0, 5.12, 6.76, 8.0);
-for my $sig_1 (@sig_1s) {
-  trials($proto_arg_string, $n_reps, $n_generations, $sig_1, $n_dimensions);
+#@sig_1s = (0.128, 0.164, 0.2, 0.256, 0.328, 0.4, 0.512); #, 0.676, 0.8, 1.0, 1.28, 1.64, 2.0, 3.28, 4.0, 5.12, 6.76, 8.0);
+#my $sig_1 = $prop_sig_1;
+my @params = (0.3, 0.5, 0.9);
+for my $param_val (@params) {
+  trials($n_reps, $proto_arg_string, $param_val);
 }
 
 sub trials{
-  my $proto_arg_string = shift;
   my $n_reps = shift;
-  my $n_generations = shift;
-  my $sig1 = shift;
-  my $n_dimensions = shift;
-  my $the_arg_string = $proto_arg_string;
-  $the_arg_string =~ s/NGENERATIONS/$n_generations/;
-  $the_arg_string =~ s/SIGMA1/$sig1/;
-# print "FHFHFHF: ", $the_arg_string, "\n";
-  #my @avg_effs = (0, 0, 0, 0);
+  my $proto_arg_string = shift;
+  my $param_val = shift;
 
+  my $the_arg_string = $proto_arg_string;
+#  print "$proto_arg_string \n";
+  $the_arg_string =~ s/MULTI/$param_val/;
+
+  my @n_tries = ();
   my @n_accepts = ();
   my @ms_dmus = ();
   my @ksds = ();
-  my @adss = ();
-  my @mean_qs = ();
+  my @efdss = ();
+  my @sq_of_mean_qs = ();
+  my @mean_ps = ();
 
   my @ndim_tvds = ();
   my @orthant_tvds = ();
   my @refl_tvds = ();
-  my @onedim_tvds = ();
+  my @onedim1_tvds = ();
+  my @onedimall_tvds = ();
 
   my %gen_avgdmusq = ();
-my %gen_avgksd = ();
-my %gen_avgads = ();
-my %gen_avgorthtvd = ();
-my %gen_avg1dimtvd = ();
- 
+  my %gen_avgksd = ();
+  my %gen_avgefds = ();
+  my %gen_avgorthtvd = ();
+  my %gen_avg1dim1tvd = ();
+  my %gen_avg1dimalltvd = ();
+#  print "# the arg string: $the_arg_string \n";
   #  my $mu = 0.0; # get from run_params file - may not always be zero!
   my %dmus = (); # $dmus{$i} is array_ref to array of dmus for component $i
-  for (0..$n_dimensions-1) {
-    $dmus{$_} = [];		# $mu[$_] = 0;
-  }
+  # for (0..$n_dimensions-1) {
+  #   $dmus{$_} = [];		# $mu[$_] = 0;
+  # }
   for (1..$n_reps) {
     my $seed = int(rand(1000000));
+ #     print "~/mcmc_toy/src/mcmc_toy  $the_arg_string  $seed  'mcmc' \n";
     system "~/mcmc_toy/src/mcmc_toy  $the_arg_string  $seed  'mcmc' >  mcmc_toy_samples_tmp";
-    # my $run_params = `cat run_params`;
-    # my @runparam_lines = split("\n", $run_params);
-    # my ($T, $shape, $s1, $s2, $p1);
-    #     for(@runparam_lines){
-    #       if(/T, Proposal\S+:\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/){
-    # 	($T, $shape, $s1, $s2, $p1) = ($1, $2, $3, $4, $5);
-    #       }
-    #     }
-my ($n_gens, $n_accept, $dmu_sq, $ksd, $ads, $mean_q, $ndim_tvd, $orthant_tvd, $refl_tvd, $oned_tvd);
-    my $tvd_vs_gen_string = `cat tvd_vs_gen`;
+ 
+    #my ($n_gens, $n_accept, $dmu_sq, $ksd, $ads, $mean_q, $mean_p, $ndim_tvd, $orthant_tvd, $refl_tvd, $oned_tvd);
+    my ($n_gens, $n_try, $n_accept, $n_pi_evals, # 0-3
+	$ndim_tvd, $orthant_tvd, $refl_tvd, $oned_tvd, $onedall_tvd, # 4-8
+	$dmu_sq, $ksd, $efds, $sq_of_mean_q, $mean_p);
+    my $tvd_vs_gen_string = `cat tvd_etc_vs_gen`;
     my @tvd_vs_gen_lines = split("\n", $tvd_vs_gen_string);
-my @cols;
-    for(@tvd_vs_gen_lines){
+    my @cols;
+    for (@tvd_vs_gen_lines) {
       @cols = split(" ", $_);
-      ($n_gens, $n_accept, $dmu_sq, $ksd, $ads, $mean_q, $ndim_tvd, $orthant_tvd, $refl_tvd, $oned_tvd) = @cols[0..9];
+      ($n_gens, $n_try, $n_accept, $n_pi_evals, # 0-3
+       $ndim_tvd, $orthant_tvd, $refl_tvd, $oned_tvd, $onedall_tvd, # 4-8
+       $dmu_sq, $ksd, $efds, $sq_of_mean_q, $mean_p) # 9-13
+	= @cols[0..13];
       $gen_avgdmusq{$n_gens} += $dmu_sq;
       $gen_avgksd{$n_gens} += $ksd;
- $gen_avgads{$n_gens} += $ads;
+      $gen_avgefds{$n_gens} += $efds;
       $gen_avgorthtvd{$n_gens} += $orthant_tvd;
-      $gen_avg1dimtvd{$n_gens} += $cols[8];
+      $gen_avg1dim1tvd{$n_gens} += $oned_tvd;
+      $gen_avg1dimalltvd{$n_gens} += $onedall_tvd;
     }
 
-  #   my $last_line = `tail -1 tvd_vs_gen`;
-  # #  print $last_line;
-  #   my @cols = split(" ", $last_line);
-
-#    my $n_gens = shift @cols;
-    print "$n_gens  $sig1  ", join("  ", @cols), "\n"; 
+ #   print "$sig1 ", join("  ", @cols), "\n"; 
     die "n generations inconsistency: $n_generations, $n_gens \n" if($n_gens != $n_generations);
-  #  my $p_accept = shift @cols;
+    push @n_tries, $n_try;
     push @n_accepts, $n_accept;
- #   my $dmu_sq = shift @cols;
     push @ms_dmus, $dmu_sq;
-
-  #  my $ksd = shift @cols;
     push @ksds, $ksd;
-#  my $ads = shift @cols;
-    push @adss, $ads;
-  #  my $mean_q = shift @cols;
-    push @mean_qs, $mean_q;
-    #print "ms_dmu, ksd, mean q: $ms_dmu  $ksd  $mean_q \n";
+    push @efdss, $efds;
+    push @sq_of_mean_qs, $sq_of_mean_q;
+    push @mean_ps, $mean_p;
 
-  #  my $ndim_tvd = shift @cols;
     push @ndim_tvds, $ndim_tvd;
- #   my $orthant_tvd = shift @cols;
     push @orthant_tvds, $orthant_tvd;
- #   my $refl_tvd = shift @cols;
     push @refl_tvds, $refl_tvd;
 
-push @onedim_tvds, $oned_tvd;
-    # for (my $i=0; $i<$n_dimensions; $i++) {
-    #   my $oned_tvd = shift @cols;
-    #   push @onedim_tvds, $oned_tvd;
-    # }
-   
-  }		  # loop over reps
-open my $fh1, ">", "avg_nreps_tvdetc_vs_gen_sig1_" . $sig1;
-my @sgens = sort {$a <=> $b} keys %gen_avgdmusq;
+    push @onedim1_tvds, $oned_tvd;
+    push @onedimall_tvds, $oned_tvd;
+  }				# loop over reps
+  open my $fh1, ">", "avg_nreps_tvdetc_vs_gen_param_" . $param_val;
+  my @sgens = sort {$a <=> $b} keys %gen_avgdmusq;
 
-  for(@sgens){
-    printf $fh1 "%8i %10.7g %10.7g %10.7g %10.7g %10.7g \n", $_,
-      $gen_avgdmusq{$_}/$n_reps, $gen_avgksd{$_}/$n_reps, $gen_avgads{$_}/$n_reps,
-      $gen_avgorthtvd{$_}/$n_reps, $gen_avg1dimtvd{$_}/$n_reps;
+  for (@sgens) {
+    printf $fh1 "%8i %10.7g %10.7g %10.7g %10.7g %10.7g %10.7g \n", $_,
+      $gen_avgdmusq{$_}/$n_reps, $gen_avgksd{$_}/$n_reps, $gen_avgefds{$_}/$n_reps, # cols 2-4
+	$gen_avgorthtvd{$_}/$n_reps, $gen_avg1dim1tvd{$_}/$n_reps,  $gen_avg1dimalltvd{$_}/$n_reps;
   }
-close $fh1;
+  close $fh1;
 
   #print "ms dmus: ", join("; ", @ms_dmus), "\n";
   #print mean(@ms_dmus), "  ", stddev(@ms_dmus), "\n";
-  printf("%8i %10.7g ", $n_generations, $sig1);
+  printf("%10.7g ", $param_val);
+  printf("%8i  ", $n_generations);
   printf("%10.7g +- %10.7g ", mean(@n_accepts)/$n_generations, stddev(@n_accepts)/$n_generations/sqrt(scalar @n_accepts));
-  printf("%10.7g +- %10.7g ", mean(@ms_dmus), stddev(@ms_dmus)/sqrt(scalar @ms_dmus));
- printf("%10.7g +- %10.7g ", mean(@ksds), stddev(@ksds)/sqrt(scalar @ksds));
- printf("%10.7g +- %10.7g ", mean(@adss), stddev(@adss)/sqrt(scalar @adss));
-  printf("%10.7g +- %10.7g ", mean(@mean_qs), stddev(@mean_qs)/sqrt(scalar @mean_qs));
 
   printf("%10.7g +- %10.7g ", mean(@ndim_tvds), stddev(@ndim_tvds)/sqrt(scalar @ndim_tvds));
   printf("%10.7g +- %10.7g ", mean(@orthant_tvds), stddev(@orthant_tvds)/sqrt(scalar @orthant_tvds));
   printf("%10.7g +- %10.7g ", mean(@refl_tvds), stddev(@refl_tvds)/sqrt(scalar @refl_tvds));
-  printf("%10.7g +- %10.7g ", mean(@onedim_tvds), stddev(@onedim_tvds)/sqrt(scalar @onedim_tvds));
+  printf("%10.7g +- %10.7g ", mean(@onedim1_tvds), stddev(@onedim1_tvds)/sqrt(scalar @onedim1_tvds));
+  printf("%10.7g +- %10.7g ", mean(@onedimall_tvds), stddev(@onedimall_tvds)/sqrt(scalar @onedimall_tvds));
 
-  # for (0..$n_dimensions-1) {
-  #   printf("%10.7g +- %10.7g ", mean(@{$dmus{$_}}), stddev(@{$dmus{$_}})/sqrt(scalar @{$dmus{$_}}) );
-  # }
+  printf("%10.7g +- %10.7g ", mean(@ms_dmus), stddev(@ms_dmus)/sqrt(scalar @ms_dmus));
+  printf("%10.7g +- %10.7g ", mean(@ksds), stddev(@ksds)/sqrt(scalar @ksds));
+  printf("%10.7g +- %10.7g ", mean(@efdss), stddev(@efdss)/sqrt(scalar @efdss));
+  printf("%10.7g +- %10.7g ", mean(@sq_of_mean_qs), stddev(@sq_of_mean_qs)/sqrt(scalar @sq_of_mean_qs));
+  printf("%10.7g +- %10.7g ", mean(@mean_ps), stddev(@mean_ps)/sqrt(scalar @mean_ps));
+
   print "\n\n";
 }
