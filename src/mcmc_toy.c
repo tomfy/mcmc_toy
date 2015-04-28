4791122fb5260aeb4f537dfc772d44edf2be972d
Thomas Fisher-York <tly2@cornell.edu>
2015-04-27 22:25:34 -0400

    removed many lines commented code.

--- src/mcmc_toy.c
+++ src/mcmc_toy.c
@@ -35,9 +35,6 @@ int main(int argc, char* argv[]){
   
   int n_dimensions = atoi(argv[1]);
   fprintf(g_run_params_fstream,"# n_dimensions: %i \n", n_dimensions);
-
-  //  Target_1dim* targ_1d = (Target_1dim*)malloc(sizeof(Target_1dim));
-  //  targ_1d->n_modes = 
     int n_peaks = atoi(argv[2]);
   fprintf(g_run_params_fstream,"# n_peaks: %i \n", n_peaks);
   //Target_peak_1dim* 
@@ -73,27 +70,20 @@ next_arg--;
     temperatures[j] = atof(argv[next_arg]); next_arg++;
     rates[j] = atof(argv[next_arg]); next_arg++;
     proposals[j].shape = argv[next_arg]; next_arg++;
-    //   fprintf(g_run_params_fstream,"%s \n", proposals[j].shape);
     proposals[j].W1 = atof(argv[next_arg]); next_arg++;
     proposals[j].W2 = atof(argv[next_arg]); next_arg++;
     proposals[j].p1 = atof(argv[next_arg]); next_arg++;
     fprintf(g_run_params_fstream,"#  T, rate, Proposal(shape,W1,W2,p1):  %8.5f %8.5f  %12s %8.5f %8.5f %8.5f \n", 
 	    temperatures[j], rates[j], proposals[j].shape, proposals[j].W1, proposals[j].W2, proposals[j].p1);
   }
-  //  next_arg += 5*n_temperatures;
   int n_bins = atoi(argv[next_arg]);
-  //printf("n bins: %i \n", n_bins);
   int n_bins_1d = atoi(argv[next_arg+1]);
   n_bins += (n_bins % 2); // to make it even
   n_bins_1d += (n_bins_1d %2); 
   fprintf(g_run_params_fstream,"#  n_bins: %i n_bins_1d: %i \n", n_bins, n_bins_1d);
-  //printf("#  n_bins: %i n_bins_1d: %i \n", n_bins, n_bins_1d);
   int seed = atoi(argv[next_arg+2]);
   const char* chain_type = argv[next_arg+3];
-  //printf("seed, type: %i  %s \n", seed, chain_type);
-  // ******************************************************************
-  //  normalize_targ_1dim(g_n_modes, g_peaks); //
-
+ 
   // *************** set up RNG *****************
 
   gsl_rng_env_setup();
@@ -104,15 +94,7 @@ next_arg--;
   // *************** open output files *************
   g_tvd_vs_gen_fstream = fopen("tvd_etc_vs_gen", "w");
 
-  //g_Ngrid_max += g_Ngrid_max % 2; // if odd, add one, to make it be even.
-
   // /*  ********** get bin boundaries ****************
-  /* const Binning_spec* the_bins = //construct_binning_spec(n_bins, targ_1d); */
-  /*    construct_binning_spec(n_bins, targ_1d, -INFINITY, INFINITY); // Use for Ndim histograms */
-  /* const Binning_spec* two_bins = construct_binning_spec(2, targ_1d, -INFINITY, INFINITY); // bin for each orthant */
-  /* const Binning_spec* one_orthant_bins = construct_binning_spec(n_bins/2, targ_1d, 0, INFINITY);  */
-  /* const Binning_spec* one_dim_bins = construct_binning_spec(2*n_bins, targ_1d, -INFINITY, INFINITY); */
-
   Binning_spec_set the_bin_set;
   the_bin_set.ndim_bins =  construct_binning_spec(n_bins, targ_1d, -INFINITY, INFINITY); // Use for Ndim histograms
   the_bin_set.onedim_bins = construct_binning_spec(n_bins_1d, targ_1d, -INFINITY, INFINITY);
@@ -131,19 +113,12 @@ next_arg--;
   g_targprobs_orthants = targprobs_orthants;
   Ndim_histogram* targprobs_one_orthant = init_target_distribution(n_dimensions, n_bins/2, 1, the_bin_set.positive_bins);
   g_targprobs_one_orthant = targprobs_one_orthant;
-  //
-  // printf("XXX\n");
-  // double tvd_sum = 0.0;
-  //  double tvdsq_sum = 0.0;
-
-  //  for(int j=0; j<n_replicates; j++){
+  
     State** states = (State**)malloc(n_temperatures*sizeof(State*));
     for(int i=0; i<n_temperatures; i++){
       states[i] = construct_state(n_dimensions, targ_1d, temperatures[i]);
     }
-    printf("before construct_multi_T...\n");
     Multi_T_chain* multi_T_chain = construct_multi_T_chain(n_temperatures, temperatures, rates, proposals, states, &the_bin_set, chain_type);
-    // printf("after construct_multi_T...\n");
     g_n_pi_evaluations = 0;
     g_max_pi_evaluations = mcmc_steps; // for now
   // ********** do burn-in ***********
@@ -153,34 +128,17 @@ next_arg--;
 
     // ********** do post-burn-in *********
     for(int n=1; n<=mcmc_steps; n++){   
-      // take a mcmc step
+      // take an mcmc step
       multi_T_chain_within_T_mcmc_step(multi_T_chain);  
-
-      //   printf("after multiT withinT step \n");
       for(int i=0; i<n_temperatures-1; i++){
 	for(int j=i+1; j<n_temperatures; j++){
-	  //  printf("i, j: %i %i \n", i, j);
 	  multi_T_chain_T_swap_mcmc_step(multi_T_chain, i, j);
-	  //  printf("after multiT T swap...\n");
 	}
       } 
-      //  fprintf(stderr, "n: %i \n", n);
       if(g_n_pi_evaluations >= g_max_pi_evaluations){ break; }
     }
-    //  printf("ZZZZZZZZZZ before multi_T_chain_output_tvd.\n");
     multi_T_chain_output_tvd(multi_T_chain);
-
-    /* tvd_sum += tvd; */
-    /* tvdsq_sum += tvd*tvd; */
-    // printf("before free_multi...\n");
     free_multi_T_chain(multi_T_chain);
-    //  } // end loop over reps
-  // printf("after reps loop...\n");
-  /* double tvd_avg = tvd_sum/n_replicates; */
-  /* double tvd_variance = tvdsq_sum/n_replicates - tvd_avg*tvd_avg; */
-  /* double tvd_stderr = sqrt(tvd_variance)/sqrt((double)n_replicates); */
-  //      printf("%4i  %8i  %10.6f +- %10.6f  %10.6f  %10.6f \n", k, n_jumps, tvd_avg, tvd_stderr, (double)Naccept_sum/(Naccept_sum+Nreject_sum), sqrt(sum_dsq/(n_replicates*mcmc_steps)));
-  //	printf("\n\n");
   fflush(stdout);
   free_ndim_histogram(targprobs_ndim);
  free_ndim_histogram(targprobs_1dim);
@@ -192,8 +150,7 @@ next_arg--;
 
 // propose uniformly from cube or ball centered on present point
 double* propose(int n_dim, double* x_array, Proposal* prop, int* which_prop){
-  double* prop_x_array = (double*)malloc(n_dim*sizeof(double));
-  //  printf("porposal:: %s  %8.5f %8.5f %8.5f \n", prop->shape, prop->W1, prop->W2, prop->p1); 
+  double* prop_x_array = (double*)malloc(n_dim*sizeof(double)); 
   double Width; //  = (drand() < prop->p1)? prop->W1 : prop->W2;
   if(drand() < prop->p1){
     Width = prop->W1;
@@ -212,7 +169,6 @@ double* propose(int n_dim, double* x_array, Proposal* prop, int* which_prop){
       double sum_sq = 0;
       for(int i = 0; i<n_dim; i++){
 	double delta_x = (double)(2*Width*drand()) - Width;
-	//   printf("width: %12.5g  delta_x: %12.5g \n", Width, delta_x);
 	prop_x_array[i] = x_array[i] + delta_x;
 	sum_sq += delta_x*delta_x;
       }
@@ -342,12 +298,6 @@ Ndim_histogram* init_target_distribution(int Ndim, int Ngrid_max, int normalize,
   return targ_bin_probs;
 }
 
-/* double total_variation_distance(const Ndim_histogram* targp, const Ndim_histogram* mcmc_out){ */
-/*   // both distributions must be normalized ahead of time */
-/*    double tvd = 0.5 * sum_abs_difference_ndim_arrays_of_double(targp->weights, mcmc_out->weights);  */
-/*   return tvd; */
-/* } */
-
 double total_variation_distance(const Ndim_histogram* targprobs, const Ndim_histogram* hist){
    Ndim_histogram* hist_copy = construct_copy_ndim_histogram(hist);
   normalize_ndim_histogram(hist_copy);
@@ -367,7 +317,7 @@ void print_array_of_int(int Nsize, int* array){
 void print_array_of_double(int Nsize, double* array){
   for(int i=0; i<Nsize; i++){
     printf("%11.8g ", array[i]);
-  }// printf("\n");
+  }
 }
 
 double draw_1dim(const Target_1dim* targ_1d, double temperature){
@@ -448,7 +398,6 @@ double* merge_sorted_arrays(const int size1, const double* a1, const int size2, 
       }
     }
     i++;
-    //    printf("%i %i %i %i %i \n", size1, size2, i, i1, i2);
   }
   return ma;
 }
@@ -589,75 +538,3 @@ void add_arrays(int size, double* sum_x, const double* x){ // add second array t
 		sum_x[i] += x[i];
 	}
 }
-
-/* double* merge_sorted_arrays(const int size1, const double* a1, const int size2, const double* a2){ */
-/*   // allocates array of size size1+size2, merges two arrays a1 and a2, which must be already sorted */
-/*   // from smallest to largest (i.e. a1[0] <= a1[1], etc.) */
-/*   double * ma = (double*)malloc((size1 + size2)*sizeof(double)); */
-/*   int i=0, i1=0, i2=0; */
-/*   while(i1 < (size1 + size2) ){ */
-/*     if(a1[i1] <= a2[i2]){ */
-/*       ma[i] = a1[i1]; */
-/*       i1++; */
-/*     }else{ */
-/*       ma[i] = a2[i2]; */
-/*       i2++; */
-/*     } */
-/*     i++; */
-/*   } */
-/*   return ma; */
-/* } */
-
-// ***** unused/obsolete stuff ****
-
-/* double draw_1dim(int n_mode, Target_peak_1dim* peaks){ */
-/*   double p = 0.0; */
-/*   double drnd = drand(); */
-/*   for(int i=0; i<n_modes; i++){ */
-/*     p += peaks[i].weight; */
-/*     if(drnd < p){ // draw from i_th peak */
-/*       return gsl_ran_gaussian(the_rng, peaks[i].sigma) + peaks[i].position; */
-/*     } */
-/*   } */
-/*   // shouldn't get here if normalization is right, but in case ... */
-/*   return gsl_ran_gaussian(the_rng, peaks[n_modes-1].sigma) + peaks[n_modes-1].position; */
-/* } */
-
-
-/* int* ipropose(int Ndim, int* index_array, Proposal* prop, int is_ball){ */
-/*   int* prop_index_array = (int*)malloc(Ndim*sizeof(int));  */
-/*   int Width = (drand() < prop->p1)? prop->W1 : prop->W2; */
-/*   while(1){ */
-/*     int sum_sq = 0; */
-/*     for(int i = 0; i<Ndim; i++){ */
-/*       int delta_i = (int)((2*Width+1)*drand()) - Width; */
-/*       prop_index_array[i] = index_array[i] + delta_i; */
-/*       sum_sq += delta_i*delta_i; */
-/*     } */
-/*     if((sum_sq > 0) && (!is_ball  || sum_sq <= Width*Width)) break; */
-/*   } */
-/*   return prop_index_array; */
-/* } */
-
-/* int i_from_x(double x){ // xmin, xmax, Ngrid_max are global */
-/*   int gridpoint; */
-/*   if(x <= Xmin){ */
-/*     gridpoint = 0; */
-/*   }else if(x >= Xmax){ */
-/*     gridpoint = Ngrid_max; */
-/*   }else{ */
-/*     gridpoint = (int)(Ngrid_max * (x-Xmin)/(Xmax-Xmin) + 0.5); */
-/*   } */
-/*   return gridpoint; */
-/* } */
-/* double* x_array_from_i_array(int Ndim, int* i_array){ */
-/*   double* x_array = (double*)malloc(Ndim*sizeof(double)); */
-/*   for(int i=0; i<Ndim; i++){ */
-/*     x_array[i] = x_from_i(i_array[i]); */
-/*   } */
-/*   return x_array; */
-/* } */
-/* double x_from_i(int i){ */
-/*   double x; */
-/*   return Xmin + (double)i/Ngrid_max * (Xmax - Xmin); */
-/* } */
--- src/mcmc_toy_structs.c
+++ src/mcmc_toy_structs.c
@@ -15,12 +15,9 @@
 State* construct_state(int n_dimensions, const Target_1dim* targ_1d, double temperature){ 
   State* the_state = (State*)calloc(1, sizeof(State));
   the_state->n_dimensions = n_dimensions;
-  //  the_state->ipoint = (int*)malloc(n_dimensions*sizeof(int));
   the_state->point = (double*)calloc(n_dimensions, sizeof(double));
   for(int i=0; i<n_dimensions; i++){
-    //  the_state->ipoint[i] = (int)( drand() * (Ngrid_max+1) ); 
     the_state->point = draw_ndim(n_dimensions, targ_1d, temperature); // draw from target distribution
-    // [i] = (double)( drand() * 1.0);
   }
   the_state->prob = f_ndim(targ_1d, n_dimensions, the_state->point);
   return the_state;
@@ -37,14 +34,12 @@ Single_T_chain* construct_single_T_chain(double T, double rate, Proposal P, Stat
  
   single_T_chain->current_state = S;
   single_T_chain->sum_x = (double*)calloc(S->n_dimensions, sizeof(double));
-  // single_T_chain->current_iid_state = construct_state(S->n_dimensions, g_targ_1d);
   single_T_chain->temperature = T;
   single_T_chain->rate = rate;
   single_T_chain->generation = 0;
   single_T_chain->within_T_steps_taken = 0;
   single_T_chain->n_try = 0;
   single_T_chain->n_accept = 0;
-  //  single_T_chain->n_reject = 0;
   single_T_chain->n_try_1 = 0;
   single_T_chain->n_try_2 = 0;
   single_T_chain->n_accept_1 = 0;
@@ -53,16 +48,8 @@ Single_T_chain* construct_single_T_chain(double T, double rate, Proposal P, Stat
   single_T_chain->dsq_sum = 0.0;
   single_T_chain->n_jumps = 1;
   single_T_chain->type = type;
-  //   single_T_chain->bins = bins;
-  // if(DO_TVD){
+ 
     single_T_chain->mcmc_out_hist = construct_ndim_histogram(S->n_dimensions, bins->ndim_bins);
-    //  single_T_chain->mcmc_out_hist->bins = bins;
-    //single_T_chain->mcmc_out_1d_hists = (Ndim_histogram**)malloc(S->n_dimensions*sizeof(Ndim_histogram*));
-    /* for(int i=0; i<S->n_dimensions; i++){ */
-    /*   single_T_chain->mcmc_out_1d_hists[i] = construct_ndim_histogram(1, bins->onedim_bins); */
-    /* } */
-    // Ndim_histogram* mcmc_out_1d0_hist; // 1dim, 0th component, 
-    // Ndim_histogram* mcmc_out_1dall_hist; // 1dim, all components
    single_T_chain->mcmc_out_1d0_hist = construct_ndim_histogram(1, bins->onedim_bins);
 single_T_chain->mcmc_out_1dall_hist = construct_ndim_histogram(1, bins->onedim_bins);
 
@@ -74,9 +61,7 @@ single_T_chain->mcmc_out_1dall_hist = construct_ndim_histogram(1, bins->onedim_b
     single_T_chain->old_gees = NULL;
     single_T_chain->n_next_block_gees = FIRST_SUMMARY_GEN;
     single_T_chain->next_block_gees = (double*)calloc(FIRST_SUMMARY_GEN, sizeof(double));
-    //single_T_chain->old_iidgees = NULL;
-    //  single_T_chain->recent_iidgees = (double*)malloc(FIRST_SUMMARY_GEN*sizeof(double));
-    // }
+  
   return single_T_chain;
 }
 
@@ -97,11 +82,9 @@ State* single_T_chain_mcmc_step(Single_T_chain* chain){
   int n_jumps = 0;
   double dsq = 0;
 
-  //  printf("in singleTmcmcstep. just before accept/reject.\n");
   // prop ratio is 1 for symmetric proposal
   if(strcmp(chain->type, "mcmc") == 0){ // mcmc step
     if((p_of_proposed_state_T > p_of_current_state_T) || (p_of_proposed_state_T > drand() * p_of_current_state_T)){ // accept
-      //   printf("ACCEPT\n");
       double h0max = 0.0;   // count if jumped from < 0.5 to > 0.5 in x and y
       for(int i=0; i<Ndim; i++){
 	if( ( the_state->point[i] <= h0max  && prop_x_array[i] > h0max ) 
@@ -118,11 +101,8 @@ State* single_T_chain_mcmc_step(Single_T_chain* chain){
       chain->n_accept++;
       if(which_prop == 1){ chain->n_accept_1++; }else{ chain->n_accept_2++; }
     }else{ 
-      // reject proposed move. // dsq += 0 , n_jumps += 0
-      //   chain->n_reject++;
-      //   printf("REJECT\n");
+      // reject proposed move. 
       free(prop_x_array);
-      //  printf("after free(propxarray).\n");
     }
   }else{ // do iid draw
     double* exact_xs = draw_ndim(Ndim, g_targ_1d, chain->temperature);
@@ -135,9 +115,7 @@ State* single_T_chain_mcmc_step(Single_T_chain* chain){
     the_state->point = exact_xs;
     the_state->prob = f_ndim(g_targ_1d, Ndim, exact_xs);
     chain->n_accept++;
-  }
-  //  printf("in singleT mcmc step. after accept/reject.\n");
-  
+  }  
 
   chain->dsq_sum += dsq; 
   chain->n_jumps += n_jumps;
@@ -145,7 +123,6 @@ State* single_T_chain_mcmc_step(Single_T_chain* chain){
   chain->n_try++; 
   if(which_prop == 1){ chain->n_try_1++; }else{ chain->n_try_2++; }
   chain->current_state = the_state;
-  //  printf("bottom of single T mcmc step. chain->within_T_steps_taken: %i \n", chain->within_T_steps_taken);
   return the_state;
 }
 
@@ -165,10 +142,8 @@ void single_T_chain_histogram_current_state(Single_T_chain* chain){
     free(reflected_point);
     add_data_pt_to_ndim_histogram(chain->mcmc_out_1d0_hist, 1, the_state->point); // zeroth component
     for(int i=0; i<n_dim; i++){
-      //   printf("i: %8i \n", i);
       add_data_pt_to_ndim_histogram(chain->mcmc_out_1dall_hist, 1, the_state->point+i);
     }
-    //  printf("%10.7g  %10.7g \n", chain->mcmc_out_1d0_hist->total_weight, chain->mcmc_out_1dall_hist->total_weight);
   }
 }
 
@@ -177,16 +152,12 @@ void single_T_chain_output_tvd(Single_T_chain* chain){
   double* all_gees = (double*)calloc(chain->generation, sizeof(double));
   int n_recent_gees = chain->generation - chain->n_old_gees; // number of states stored in next_block (i.e. after old_gees)
   assert(n_recent_gees == chain->n_next_block_gees || g_n_pi_evaluations >= g_max_pi_evaluations );
-  // printf("top of singleT output_tvd... gen, n_old, nrecent(s): %i %i %i %i \n", chain->generation, chain->n_old_gees, n_recent_gees, chain->n_next_block_gees);
   qsort(chain->next_block_gees, chain->n_next_block_gees, sizeof(double), cmpfunc);
   if(chain->old_gees == NULL){
     all_gees = copy_array(chain->n_next_block_gees, chain->next_block_gees);
   }else{
-    //   printf("%8i %8i %8i %8i\n",chain->n_next_block_gees, chain->n_old_gees, chain->within_T_steps_taken, chain->within_T_steps_taken-chain->n_old_gees);
     all_gees = merge_sorted_arrays(chain->n_old_gees, chain->old_gees, chain->n_next_block_gees, chain->next_block_gees);
   }
-  //printf("n old, recent, chain->recent, all gees: %i %i %i %i \n", 
-  //	 chain->n_old_gees, chain->n_next_block_gees, chain->n_next_block_gees, chain->within_T_steps_taken);
   if(fabs(chain->temperature - 1.0) < 1e-10){ // only output cold-chain info.
   fprintf(g_tvd_vs_gen_fstream, "%8i %8i %8i %8ld  ", chain->within_T_steps_taken, chain->n_try, chain->n_accept, g_n_pi_evaluations);
 
@@ -204,7 +175,6 @@ void single_T_chain_output_tvd(Single_T_chain* chain){
     fprintf(g_tvd_vs_gen_fstream, "%10.7g ", tvd);
   }
 
- // dmusq, KSD, EDFS, pow(chain->sum_q/chain->within_T_steps_taken, 2.0), chain->sum_p/chain->within_T_steps_taken
   double dmusq = 0.0;
   for(int i=0; i<chain->current_state->n_dimensions; i++){ 
     double dmu = chain->sum_x[i]/chain->within_T_steps_taken - g_targ_1d->mean; //  muhat - mu
@@ -220,16 +190,11 @@ void single_T_chain_output_tvd(Single_T_chain* chain){
 	  chain->temperature);
   fprintf(g_tvd_vs_gen_fstream, "\n");
   }
-//printf("old_gees pointer: %p \n", chain->old_gees);
  
   if(chain->old_gees != NULL){  printf("before free old_gees \n"); free(chain->old_gees); }
   chain->old_gees = all_gees;
   chain->n_old_gees = chain->generation; // chain->n_old_gees + chain->n_next_block_gees;
-//printf("before free next_block_gees. n_old_gees, with_T_steps_taken: %i %i  \n", chain->n_old_gees, chain->within_T_steps_taken);
-// printf("next_block_gees pointer: %p \n", chain->next_block_gees);
-  // for(int jj=0; jj<chain->n_next_block_gees; jj++){ printf("jj, g: %i %g \n", jj, chain->next_block_gees[jj]); }
   free(chain->next_block_gees);
-// printf("after free next_block_gees \n");
   int next_n_next_block_gees = (int)((SUMMARY_GEN_FACTOR - 1.0)*chain->n_old_gees);
   chain->n_next_block_gees = next_n_next_block_gees;
   chain->next_block_gees = (double*)calloc(next_n_next_block_gees, sizeof(double));
@@ -238,7 +203,6 @@ void single_T_chain_output_tvd(Single_T_chain* chain){
 void free_single_T_chain(Single_T_chain* chain){
   free_state(chain->current_state);
   free_ndim_histogram(chain->mcmc_out_hist);
-  //  if(DO_TVD){ free(chain->old_gees); free(chain->next_block_gees); }
   free(chain); 
 }
 
@@ -250,7 +214,6 @@ Multi_T_chain* construct_multi_T_chain(int n_temperatures, double* temperatures,
   multi_T_chain->count_within_T_updates = 0;
   multi_T_chain->coupled_chains = (Single_T_chain**)calloc(n_temperatures, sizeof(Single_T_chain*));
   for(int i=0; i<n_temperatures; i++){
-    //  printf("i: %i;  before construct single T chain. \n", i);
     multi_T_chain->coupled_chains[i] = construct_single_T_chain(temperatures[i], rates[i], proposals[i], states[i], bins, type);
   }
   return multi_T_chain;
@@ -261,9 +224,7 @@ void multi_T_chain_within_T_mcmc_step(Multi_T_chain* multi_T_chain){
   for(int i=0; i<multi_T_chain->n_temperatures; i++){
     Single_T_chain* chain = multi_T_chain->coupled_chains[i];
     if(drand() < chain->rate){
-      //  printf("about to call singleT mcmc step. i, n_old_gees: %i %i \n", i, chain->n_old_gees);
       single_T_chain_mcmc_step( chain );
-      //  printf("after call to singleT mcmc step. i,  n_old_gees: %i %i \n", i, chain->n_old_gees);
     }
     add_arrays(n_dim, chain->sum_x, chain->current_state->point);
     chain->sum_q += g_shortrange(chain->current_state);
@@ -271,8 +232,6 @@ void multi_T_chain_within_T_mcmc_step(Multi_T_chain* multi_T_chain){
     chain->generation++; // generation increments whether a particular chain does a within_T step or not.
     single_T_chain_histogram_current_state(chain);
     int j = chain->generation - chain->n_old_gees - 1;
-    //   printf("i, generation(s), n_old, n_recent, j: %i %i %i %i  %i %i  \n", 
-    //	   i, multi_T_chain->generation, chain->generation, chain->n_old_gees, chain->n_next_block_gees, j);
     assert(j < chain->n_next_block_gees && j >= 0);
     chain->next_block_gees[j] = g(chain->current_state);
   }
@@ -286,17 +245,11 @@ void multi_T_chain_within_T_mcmc_step(Multi_T_chain* multi_T_chain){
       print_array_of_double(n_dim, chain->current_state->point); printf(" ");
       printf("\n");
     }   
-    //  printf("aa n withinT, old, recent: %i %i %i \n", chain->within_T_steps_taken, chain->n_old_gees, chain->n_next_block_gees);
-      //  if(multi_T_chain->generation == multi_T_chain->next_summary_generation){
       if(chain->generation == chain->n_old_gees + chain->n_next_block_gees){
-	//	printf("XXX n withinT, old, recent: %i %i %i \n", chain->within_T_steps_taken, chain->n_old_gees, chain->n_next_block_gees);
 	single_T_chain_output_tvd(chain);
       }     
-      //  printf("end of loop over singleT chains in multiT withinT step. i: %i \n", i);
   }
   multi_T_chain->summary_generation = multi_T_chain->generation;
-  //  multi_T_chain->next_summary_generation = (int)(multi_T_chain->generation * SUMMARY_GEN_FACTOR);;
-  // printf("bottom of multiT within T step \n");
 }
 
 void multi_T_chain_T_swap_mcmc_step(Multi_T_chain* multi_T_chain, int i_c, int i_h){
@@ -431,23 +384,18 @@ double set_ndim_array_of_double_to_target(Ndim_array_of_double* array_struct,
   double sum = 0.0;
   if(Ndim == 1){
     for(int i=0; i<Nsize; i++){
-      //double x = Xmin + (double)i/(double)(Nsize-1)*(Xmax-Xmin);
-      //    printf("%8i %8.5g  %8.5g \n", i, outer_value, function(x));
       double bin_prob = 0.0;
       for(int j=0; j<targ_1d->n_modes; j++){
 	bin_prob += 
 	  peaks[j].weight * (gsl_cdf_gaussian_P(bin_boundaries[i+1]-peaks[j].position, peaks[j].sigma) 
 			     - gsl_cdf_gaussian_P(bin_boundaries[i]-peaks[j].position, peaks[j].sigma) );
       };
-      //printf("i, binboundaries[i],[i+1], bin_prob: %i %12.5g  %12.5g  %12.5g \n", i, bin_boundaries[i], bin_boundaries[i+1], bin_prob);
 
       double innermost_value =  outer_value*bin_prob;
       ((double*)array_struct->array)[i] = innermost_value;
       sum += innermost_value;
-      //   printf("%8.5f ", innermost_value);
-    }//printf("\n");
+    }
   }else{    
-    //    printf("Ndim: %i \n", Ndim);
     for(int i=0; i<Nsize; i++){  
       double bin_prob = 0.0; 
       for(int j=0; j<targ_1d->n_modes; j++){
@@ -455,15 +403,6 @@ double set_ndim_array_of_double_to_target(Ndim_array_of_double* array_struct,
 	  peaks[j].weight * (gsl_cdf_gaussian_P(bin_boundaries[i+1]-peaks[j].position, peaks[j].sigma) 
 			     - gsl_cdf_gaussian_P(bin_boundaries[i]-peaks[j].position, peaks[j].sigma) );
       };
-
-
-      /* gsl_cdf_gaussian_P(bin_boundaries[i+1]-peaks[0].position, peaks[0].sigma)  */
-      /* 	- gsl_cdf_gaussian_P(bin_boundaries[i]-peaks[0].position, peaks[0].sigma) */
-      /* 	+ gsl_cdf_gaussian_P(bin_boundaries[i+1]-peaks[1].position, peaks[1].sigma)  */
-      /* 	- gsl_cdf_gaussian_P(bin_boundaries[i]-peaks[1].position, peaks[1].sigma); */
-      // double bin_prob = gsl_cdf_gaussian_P(bin_boundaries[i+1], peaks[0].sigma) - gsl_cdf_gaussian_P(bin_boundaries[i], peaks[0].sigma)
-      //	+ gsl_cdf_gaussian_P(bin_boundaries[i+1], peaks[1].sigma) - gsl_cdf_gaussian_P(bin_boundaries[i], peaks[1].sigma);
-      //qprintf("i, binboundaries[i],[i+1], bin_prob: %i %12.5g  %12.5g  %12.5g \n", i, bin_boundaries[i], bin_boundaries[i+1], bin_prob);
 
       Ndim_array_of_double* inner_array_struct = ((Ndim_array_of_double**)array_struct->array)[i];
       sum +=  set_ndim_array_of_double_to_target(inner_array_struct, outer_value*bin_prob, targ_1d, bins);
@@ -750,60 +689,3 @@ int* i_array_from_x_array(const Binning_spec* bins, int Ndim, double* x_array){
   }
   return i_array;
 } 
-
-
-
-// obsolete/unused stuff ...
-
-/* double set_ndim_array_of_double_with_function(Ndim_array_of_double* array_struct, */
-/* 					      double outer_value, double_func_ptr function){ */
-/*   // for probs. of form Prod(i=1,ndim)(f(x_i)), give it a 1-dim function of x (in [0,1]) as the last argument */
-/*   int Ndim = array_struct->Ndim; */
-/*   int Nsize = array_struct->Nsize; */
-/*   //  printf("ZZZ %i %i \n", Ndim, Nsize); */
-/*   double sum = 0.0; */
-/*   if(Ndim == 1){ */
-/*     for(int i=0; i<Nsize; i++){ */
-/*       double x = Xmin + (double)i/(double)(Nsize-1)*(Xmax-Xmin); */
-/*       //    printf("%8i %8.5g  %8.5g \n", i, outer_value, function(x)); */
-/*       double innermost_value =  outer_value*function(x); */
-/*       ((double*)array_struct->array)[i] = innermost_value; */
-/*       sum += innermost_value; */
-/*       //   printf("%8.5f ", innermost_value); */
-/*     }//printf("\n"); */
-/*   }else{     */
-/*     for(int i=0; i<Nsize; i++){    */
-/*       double x = (double)i/(double)(Nsize-1); */
-/*       Ndim_array_of_double* inner_array_struct = ((Ndim_array_of_double**)array_struct->array)[i]; */
-/*       sum +=  set_ndim_array_of_double_with_function(inner_array_struct, outer_value*function(x), function); */
-/*     } */
-/*   } */
-/*   return sum; */
-/* } */
-
-/* double set_ndim_array_of_double_with_cdf(Ndim_array_of_double* array_struct, */
-/* 					      double outer_value){ */
-/*   // for probs. of form Prod(i=1,ndim)(f(x_i)), give it a 1-dim function of x (in [0,1]) as the last argument */
-/*   int Ndim = array_struct->Ndim; */
-/*   int Nsize = array_struct->Nsize; */
-/*   //  printf("ZZZ %i %i \n", Ndim, Nsize); */
-/*   double sum = 0.0; */
-/*   int Nmiddle = (Ngrid_max-1)/2;  */
-/*   if(Ndim == 1){ */
-/*     for(int i=0; i<Nmiddle; i++){ */
-/*       double x = Xmin + (double)i/(double)(Nsize-1)*(Xmax-Xmin); */
-/*       //    printf("%8i %8.5g  %8.5g \n", i, outer_value, function(x)); */
-/*       double innermost_value =  outer_value*function(x); */
-/*       ((double*)array_struct->array)[i] = innermost_value; */
-/*       sum += innermost_value; */
-/*       //   printf("%8.5f ", innermost_value); */
-/*     }//printf("\n"); */
-/*   }else{     */
-/*     for(int i=0; i<Nsize; i++){    */
-/*       double x = (double)i/(double)(Nsize-1); */
-/*       Ndim_array_of_double* inner_array_struct = ((Ndim_array_of_double**)array_struct->array)[i]; */
-/*       sum +=  set_ndim_array_of_double_with_function(inner_array_struct, outer_value*function(x), function); */
-/*     } */
-/*   } */
-/*   return sum; */
-/* } */
