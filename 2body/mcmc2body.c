fb4f975be1d0f2143ffca37fbd9902e38e4670af
Thomas Fisher-York <tly2@cornell.edu>
2018-11-13 11:47:39 -0500

    run length based on pi_evaluation_count, for better comparison between methods. clean-up (freeing memory).

--- 2body/mcmc2body.c
+++ 2body/mcmc2body.c
@@ -16,6 +16,7 @@
 
 gsl_rng* g_rng;
 long pi_evaluation_count; 
+int n_thin;
 
 // ************* main: ******************
 
@@ -39,12 +40,13 @@ int main(int argc, char* argv[]){
   // run arguments:
   long seed = atoi(argv[i_arg++]);
   double burn_in_arg = atof(argv[i_arg++]);
-  long n_updates = atoi(argv[i_arg++]);
-  long burn_in = (burn_in_arg > 0)? (long)burn_in_arg : (long)(fabs(burn_in_arg) * n_updates);
+  long max_n_updates = atoi(argv[i_arg++]); 
+  long max_pi_evals = atoi(argv[i_arg++]); // bail out after this many evaluations of target density.
+  long burn_in = (burn_in_arg > 0)? (long)burn_in_arg : (long)(fabs(burn_in_arg) * max_pi_evals); // burn-in defined in terms of numbers of pi evaluations.
   long n_runs = atoi(argv[i_arg++]);
 
   // output arguments:
-  int n_thin =  atoi(argv[i_arg++]);
+  n_thin =  atoi(argv[i_arg++]);
   char* output_format = (char*)malloc(32*sizeof(char)); 
   sprintf(output_format, "%s", argv[i_arg++]);
   int verbose = atoi(argv[i_arg++]);
@@ -55,6 +57,9 @@ int main(int argc, char* argv[]){
   printf("# dimensions: %i \n", the_target->n_dims);
   printf("# n_peaks: %i \n", the_target->n_peaks);
 
+  print_target_info(the_target);
+
+
   printf("# n_levels: %i \n", n_levels);
   printf("# n_per_level: %i \n", n_per_level);
   printf("# symmetry: %i \n", symmetry);
@@ -63,7 +68,9 @@ int main(int argc, char* argv[]){
   printf("# interpolation_power: %10.8g \n", interpolation_power);
 
   printf("# seed: %ld \n", seed);
-  printf("# burn-in: %ld,  updates: %ld \n", burn_in, n_updates);
+  printf("# max updates: %ld \n", max_n_updates);
+  printf("# max pi evals: %ld \n", max_pi_evals);
+  printf("# burn-in: %ld \n", burn_in);
   printf("# runs: %ld \n", n_runs);
 
   printf("# n_thin: %d \n", n_thin);
@@ -95,26 +102,31 @@ int main(int argc, char* argv[]){
 
     check_state_consistency(the_target, the_arch, the_chain_state);
 
-    // *************************************************************
-    // ********* Loop through n_updates updates ********************
 
+    
+    // *************************************************************
+    // ******************* Loop through updates ********************
+    // *************************************************************
+    long next_summary = 100;
     pi_evaluation_count = 0;
+    long n_updates_done;
     if(n_per_level == 1){ // standard 1-body heating.
 
-      for(int i = 1; i <= burn_in; i++){ // burn-in
+      for(long i = 1; i <= burn_in; i++){ // burn-in
         step_1b(the_target, the_arch, the_chain_state);
+        if(pi_evaluation_count >= burn_in){ break; }
       }
       reset_chain_state(the_chain_state);
       pi_evaluation_count = 0;
 
-      for(int i = 1; i <= n_updates; i++){ // loop through updates       
+      for(long i = 1; i <= max_n_updates; i++){ // loop through updates       
         step_1b(the_target, the_arch, the_chain_state);
         accumulate_x_sums(the_chain_state);
 
         if(i % n_thin == 0){
           // check_state_consistency(the_target, the_arch, the_chain_state);
           if(verbose){
-            printf("%5i ", i);
+            printf("%4ld %4ld ", i, pi_evaluation_count);
             if(strcmp(output_format, "T") == 0){
               print_states_T_order(the_chain_state);
             }else if(strcmp(output_format, "walker") == 0){
@@ -125,17 +137,34 @@ int main(int argc, char* argv[]){
             printf("\n");
           }
         }
+        if(pi_evaluation_count >= next_summary){ // get KSD statistic, etc.
+          vector* v = the_chain_state->all_coldx0s;
+          qsort(v->elements, v->count, sizeof(double), compare_doubles);
+          printf("KSD: %4ld ", pi_evaluation_count);
+          double avg_KSD = 0.0;
+          for(int iw = 0; iw < 2*the_chain_state->n_levels; iw++){
+            vector* vw = the_chain_state->w_coldx0s[iw];
+            qsort(vw->elements, vw->count, sizeof(double), compare_doubles);
+            double KSD = Kolmogorov_Smirnov_D_statistic_2_sample(v->count, v->elements, vw->count, vw->elements);
+            avg_KSD += KSD;
+            printf("%2d %5ld %6.4f  ", iw, vw->count, KSD); 
+          }
+          printf(" %6.4f\n", avg_KSD/(2*the_chain_state->n_levels));
+          next_summary = (long)(next_summary * 1.2);
+          if(next_summary > max_pi_evals){ next_summary = max_pi_evals; }
+        }
+        if(pi_evaluation_count >= max_pi_evals){ n_updates_done = the_chain_state->updates; break ;}
       }
-
     }else{ // 2-body auxiliary distributions
 
-      for(int i = 1; i <= burn_in; i++){ // loop through updates
+      for(long i = 1; i <= burn_in; i++){ // do the burnin
         step_2b(the_target, the_arch, the_chain_state);
+        if(pi_evaluation_count >= burn_in){ break; }
       }
       reset_chain_state(the_chain_state);
       pi_evaluation_count = 0;
 
-      for(int i = 1; i <= n_updates; i++){ // loop through updates
+      for(long i = 1; i <= max_n_updates; i++){ // loop through updates
 
         step_2b(the_target, the_arch, the_chain_state);
         accumulate_x_sums(the_chain_state);
@@ -149,7 +178,7 @@ int main(int argc, char* argv[]){
         if(i % n_thin == 0){
           check_state_consistency(the_target, the_arch, the_chain_state);
           if(verbose){
-            printf("%5i ", i);
+            printf("%4ld %4ld ", i, pi_evaluation_count);
             if(strcmp(output_format, "T") == 0){
               print_states_T_order(the_chain_state);
             }else if(strcmp(output_format, "walker") == 0){
@@ -160,43 +189,45 @@ int main(int argc, char* argv[]){
             printf("\n");
           }
         }
-      }
-    } // end of loop through updates
+        if(pi_evaluation_count >= max_pi_evals){ n_updates_done = the_chain_state->updates; break ;}
+      } // end of loop through updates
+    }
+
     // *********************************************
 
     if(i_run == 0){
       printf("# x-update acceptance rates for each walker: \n# ");
       for(int iw = 0; iw < 2*n_levels; iw++){
-        printf("  %2i %5.3f", iw, the_chain_state->w_accepts[iw]/(1.0*n_updates));
+        printf("  %2i %5.3f", iw, the_chain_state->w_accepts[iw]/(1.0*n_updates_done));
       }printf("\n");
 
       printf("# X-move acceptance rates:\n");
       for(int it = 0; it < n_levels; it++){
         if(n_per_level == 2){
           printf("#   T-level: %1i P_A: L, R, avg: %5.3f %5.3f \n", it,          
-                 the_chain_state->t_Laccepts[it]/(1.0*n_updates),
-                 the_chain_state->t_Raccepts[it]/(1.0*n_updates)
+                 the_chain_state->t_Laccepts[it]/(1.0*n_updates_done),
+                 the_chain_state->t_Raccepts[it]/(1.0*n_updates_done)
                  ); 
         }else{
           printf("#   T-level: %1i P_A:  %5.3f %5.3f %5.3f \n", it,   
-                 the_chain_state->t_Laccepts[it]/(1.0*n_updates),
-                 the_chain_state->t_Raccepts[it]/(1.0*n_updates),       
-                 the_chain_state->t_accepts[it]/(2.0*n_updates) );
+                 the_chain_state->t_Laccepts[it]/(1.0*n_updates_done),
+                 the_chain_state->t_Raccepts[it]/(1.0*n_updates_done),       
+                 the_chain_state->t_accepts[it]/(2.0*n_updates_done) );
         }
       }
 
       printf("# T-swap acceptance rates:\n");
       for(int it = 0; it < n_levels-1; it++){
         printf("#   T-levels: %1i-%1i P_A L,R,both: %5.3f %5.3f %5.3f \n", it, it+1, 
-               the_chain_state->t_Tswap_Laccepts[it]/(1.0*n_updates),
-               the_chain_state->t_Tswap_Raccepts[it]/(1.0*n_updates),
-               the_chain_state->t_Tswap_accepts[it]/(2.0*n_updates)); 
+               the_chain_state->t_Tswap_Laccepts[it]/(1.0*n_updates_done),
+               the_chain_state->t_Tswap_Raccepts[it]/(1.0*n_updates_done),
+               the_chain_state->t_Tswap_accepts[it]/(2.0*n_updates_done)); 
       }
 
       printf("# LR-swap acceptance rates:\n");
       for(int it = 0; it < n_levels; it++){
         printf("#   T-level: %1i P_A: %5.3f \n", it,          
-               the_chain_state->t_LRswap_accepts[it]/(1.0*n_updates)
+               the_chain_state->t_LRswap_accepts[it]/(1.0*n_updates_done)
                ); 
       }
 
@@ -204,11 +235,11 @@ int main(int argc, char* argv[]){
         printf("# Level: %2i LR Xavg: ", it);
         double* Lxsum = the_chain_state->t_Lxsums[it];
         for(int i_dim = 0; i_dim < the_target->n_dims; i_dim++){
-          printf("%8.5f ", Lxsum[i_dim] / n_updates);
+          printf("%8.5f ", Lxsum[i_dim] / n_updates_done);
         }printf("   ");
         double* Rxsum = the_chain_state->t_Rxsums[it];
         for(int i_dim = 0; i_dim < the_target->n_dims; i_dim++){
-          printf("%8.5f ", Rxsum[i_dim] / n_updates);
+          printf("%8.5f ", Rxsum[i_dim] / n_updates_done);
         }printf(" \n");
       }
 
@@ -221,15 +252,19 @@ int main(int argc, char* argv[]){
     //  printf("L abserrors:  ");
     printf("# Abs Errors: ");
     for(int it = 0; it < n_levels; it++){
-      printf("%8.6g ", error(the_target->n_dims, the_target->mean_x, the_chain_state->t_Lxsums[it], n_updates));
+      printf("%8.6g ", error(the_target->n_dims, the_target->mean_x, the_chain_state->t_Lxsums[it], n_updates_done));
     }printf("  ");
     //   printf("R abserrors:  ");
     for(int it = 0; it < n_levels; it++){
-      printf("%8.6g ", error(the_target->n_dims, the_target->mean_x, the_chain_state->t_Rxsums[it], n_updates));
+      printf("%8.6g ", error(the_target->n_dims, the_target->mean_x, the_chain_state->t_Rxsums[it], n_updates_done));
     }printf("\n");
     printf("# Pooled Abs Error: %10.8g \n", error(the_target->n_dims, the_target->mean_x, est_mean_pi(the_arch, the_chain_state), 1)); 
-    free(the_chain_state);
-  }
+
+  // ********** clean up **************
+  free_chain_state(the_chain_state);
+
+  } // end of loop over runs.
+
 
   
 } // end of main
@@ -265,6 +300,7 @@ double* est_mean_pi(chain_architecture* arch, chain_state* state){ // get the 'b
       printf("# 2bodyB: xavg[0]: %10.8g \n", xavg[0]);
     }
   }
+  printf("# estimated mean x[0]: %10.7g \n", xavg[0]);
   return xavg;
 }
 
